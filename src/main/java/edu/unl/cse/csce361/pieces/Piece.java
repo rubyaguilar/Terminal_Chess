3db65940f557530f643d23f509a671c3d0750920
Emily Ewalt <ecewalt@gmail.com>
2019-08-03 16:30:40 -0500

    added beginning of checkIfSpotOpen validity method for Piece

--- src/main/java/edu/unl/cse/csce361/pieces/Piece.java
+++ src/main/java/edu/unl/cse/csce361/pieces/Piece.java
@@ -1,5 +1,6 @@
 package edu.unl.cse.csce361.pieces;
 
+import edu.unl.cse.csce361.board.Board;
 import edu.unl.cse.csce361.board.ColorSet;
 import edu.unl.cse.csce361.board.Location;
 
@@ -21,5 +22,30 @@ abstract class Piece {
 	
 	public Location getLocation() {
 		return location;
+	}
+	
+	/**
+	 * Method to help break up the valid move checking
+	 * @param location
+	 * @return 	true if you can continue with valid move checking
+	 * 			false if already definitely invalid move
+	 */
+	// TODO: Rook/King may have to override this, with their unique move swap
+	protected boolean checkIfSpotOpen(Location location) {
+		String spot = Board.getBoard().getSpot(location);
+		
+		// split string on "" and will give each individual character
+		// "A6" array.size == 2
+		// if you split unicode character like that array.size == 1
+		boolean isUnicode = spot.split("").length == 1 ? true : false;
+		
+		// if the spot isn't unicode, must just be a string dictating the spot is "E3" or something
+		// which means you can move on with your move validity checking
+		// just return true
+		if(!isUnicode) {
+			return true;
+		}
+		
+		return true;
 	}
 }
