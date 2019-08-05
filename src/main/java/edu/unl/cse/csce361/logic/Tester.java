3b0525a6c99f67c101bd99f5cc6e80741710cf5f
rubenag025 <rubenag025@gmail.com>
2019-08-05 11:30:20 -0500

    updates to tester for point class

--- src/main/java/edu/unl/cse/csce361/logic/Tester.java
+++ src/main/java/edu/unl/cse/csce361/logic/Tester.java
@@ -9,31 +9,31 @@ public class Tester {
 	 */
 	
 	public static void welcome() {
-		System.out.println("Welcome to CSCE361 Chess!\n");
-		System.out.println("To begin, decide who the first player is. First player, select the piece you'd like to move\n");
+		System.out.println("*************************");
+		System.out.println("Welcome to CSCE361 Chess!");
+		System.out.println("*************************");
+		System.out.println("To begin, decide who the first player is. \nFirst player, select the piece you'd like to move.\n");
 		System.out.println("Instructions: \n1. Select a piece by entering its associated vertical letter followed by its associated horizontal number");
-		System.out.println("Example: a6\n");
+		System.out.println("Example: What piece would you like move?: a6\n");
 		System.out.println("2. Select your desired location by entering its associated vertical letter followed by its associated horizontal number");
-		System.out.println("Example: h8\n");
+		System.out.println("Example: What where would you like to move?: h8\n");
 	}
-	
 	
 	public static void getMove() {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("What piece would you like move?: ");
 		
 		String tempPiece = scan.nextLine();
-		Point piece = new Point(tempPiece);
+		Point piece = new Point(tempPiece); //piece they want to move
 			
 		
 		System.out.print("\nWhat where would you like to move?: ");
 		String desiredMove = scan.next();
 		Point move = new Point(desiredMove); //where they want to move 
-		
 	}
 
 	public static void main(String[] args) {
-		welcome();
-		getMove();
+		Tester.welcome();
+		Tester.getMove();
 	}
 }
