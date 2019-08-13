a2e1c21f5821f82de337372871b18f55a27c5335
szhao16 <szhao16@huskers.unl.edu>
2019-08-13 03:24:30 +0000

    fixed the problem of the move rules for the piece in Board.java.

--- src/main/java/edu/unl/cse/csce361/board/Board.java
+++ src/main/java/edu/unl/cse/csce361/board/Board.java
@@ -141,28 +141,40 @@ public class Board {
 		switch(str) {
 		case "\u2654":
 			piece = new King(ColorSet.WHITE,p);
+			break;
 		case "\u2655":
 			piece = new Queen(ColorSet.WHITE,p);
+			break;
 		case "\u2656":
 			piece = new Rook(ColorSet.WHITE,p);
+			break;
 		case "\u2657":
 			piece = new Bishop(ColorSet.WHITE,p);
+			break;
 		case "\u2658":
 			piece = new Knight(ColorSet.WHITE,p);
+			break;
 		case "\u2659":
 			piece = new Pawn(ColorSet.WHITE,p);
+			break;
 		case "\u265A":
 			piece = new King(ColorSet.BLACK,p);
+			break;
 		case "\u265B":
 			piece = new Queen(ColorSet.BLACK,p);
+			break;
 		case "\u265C":
 			piece = new Rook(ColorSet.BLACK,p);
+			break;
 		case "\u265D":
 			piece = new Bishop(ColorSet.BLACK,p);
+			break;
 		case "\u265E":
 			piece = new Knight(ColorSet.BLACK,p);
+			break;
 		case "\u265F":
 			piece = new Pawn(ColorSet.BLACK,p);
+			break;
 		
 		}
 		return piece;
