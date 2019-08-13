50172b46120254af5b149187290cce2125dbb7b4
sshaikh <sshaikh@huskers.unl.edu>
2019-08-13 15:49:27 -0500

    Fixed issue of pawn moving 2 spaces first move only

--- src/main/java/edu/unl/cse/csce361/pieces/Pawn.java
+++ src/main/java/edu/unl/cse/csce361/pieces/Pawn.java
@@ -2,13 +2,16 @@ package edu.unl.cse.csce361.pieces;
 
 import edu.unl.cse.csce361.board.ColorSet;
 import edu.unl.cse.csce361.logic.Point;
+import edu.unl.cse.csce361.board.Board;
 
 
 public class Pawn extends Piece {
 	private boolean firstStep;
 	public Pawn(ColorSet color, Point Point) {
 		super(color, Point);
-		this.firstStep=true;
+		if(Point.getRowNumber() == 6 || Point.getRowNumber() == 2) {
+			this.firstStep = true;
+		}
 		// TODO Auto-generated constructor stub
 	}
 
@@ -20,17 +23,14 @@ public class Pawn extends Piece {
 		int originalColumn = originaPoint.getColumnLetter();
 		int x = Math.abs(row-originalRow);
 		int y = Math.abs(column-originalColumn);
-			
-		if(this.firstStep==true) {
-			if(x==1&&y==0||x==2&&y==0)
+		
+		if(this.firstStep == true && x == 2 && y == 0) {
+			return true;
+		}else {
+			if(x == 1 && y == 0) {
 				return true;
-			this.firstStep=false;
+			}
 		}
-		else {
-			if(x==1&&y==0)
-				return true;
-		}
-	
 		return false;
 	}
 
